28abf89ae9c96ac0797b3f8803e67624e19310c2
Aditsharma12 <159578741+Aditsharma12@users.noreply.github.com>
2024-07-22 12:51:56 +0530

    Update ok.js

--- ok.js
+++ ok.js
@@ -1,13 +1,12 @@
 document.getElementById('name').addEventListener('click', function() {
-    let name = prompt("Please enter your name");
+    let name = prompt("Please enter name");
+    document.getElementById('date').addEventListener('click', function() {
+        let date = prompt("Please enter date/month/year");
+        if (date !== null) {
+            document.getElementById('date1').innerText = mark this great date ${date} on calender\n as its greatness is overwhelming;
+        }
     if (name !== null) {
-        document.getElementById('name1').innerText = `HAPPY BIRTHDAY ${name}!`;
+        document.getElementById('name1').innerText = HAPPY BIRTHDAY ${name}!;
     }
 });
-
-document.getElementById('date').addEventListener('click', function() {
-    let date = prompt("Please enter date/month/year");
-    if (date !== null) {
-        document.getElementById('date1').innerText = `Mark this great date ${date} on your calendar as its greatness is overwhelming.`;
-    }
 });
